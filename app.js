4abb97217dfb60be0b20e257bd24713ef732676f
catalinamus <163447215+catalinamus@users.noreply.github.com>
2026-04-16 14:43:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ contenedor.innerHTML += `
     <h3>${perro.nombre}</h3>
     <p>${perro.edad}</p>
     <p>${perro.descripcion}</p>
-    <a href="https://wa.me/549XXXXXXXXXX">
+    <a href="https://wa.me/1135746150">
       <button>Quiero adoptarlo 💛</button>
     </a>
   </div>
