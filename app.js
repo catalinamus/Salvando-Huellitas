307a85740e330ed80edc853d70471dbfdebb6d47
catalinamus <163447215+catalinamus@users.noreply.github.com>
2026-04-16 15:13:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,22 +2,35 @@ const enAdopcion = [
   {
     nombre: "Trufa",
     edad: "10 años",
-    descripcion: "Súper mimosa y amorosa",
+    descripcion: "Súper mimosa",
     imagen: "https://i.imgur.com/PQUMkyU.jpeg"
+  },
+  {
+    nombre: "Miel",
+    edad: "8 meses",
+    descripcion: "Amorosa y juguetona",
+    imagen: "https://i.imgur.com/XXXX.jpg"
+  },
+  {
+    nombre: "Panqueque",
+    edad: "4 años",
+    descripcion: "Resiliente y compañero",
+    imagen: "https://i.imgur.com/XXXX.jpg"
   }
 ];
+
 const contenedor = document.getElementById("adopcion");
 
 enAdopcion.forEach(perro => {
-contenedor.innerHTML += `
-  <div class="card">
-    <img src="${perro.imagen}">
-    <h3>${perro.nombre}</h3>
-    <p>${perro.edad}</p>
-    <p>${perro.descripcion}</p>
-    <a href="https://wa.me/5491135746150">
-      <button>Quiero adoptarlo 💛</button>
-    </a>
-  </div>
-`;
+  contenedor.innerHTML += `
+    <div class="card">
+      <img src="${perro.imagen}">
+      <h3>${perro.nombre}</h3>
+      <p>${perro.edad}</p>
+      <p>${perro.descripcion}</p>
+      <a href="https://wa.me/5491135746150">
+        <button>Adoptar 💛</button>
+      </a>
+    </div>
+  `;
 });
